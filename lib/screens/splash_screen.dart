8613c1739ddecb1b1beacbd47375016f36e8064c
kamau-David <dayvieek@gmail.com>
2026-01-28 23:02:30 +0300

    initial of the animated splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../main.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  double _opacity = 0;
+  double _scale = 0.5;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(milliseconds: 300), () {
+      if (mounted) {
+        setState(() {
+          _opacity = 1.0;
+          _scale = 1.0;
+        });
+      }
+    });
+
+    Timer(const Duration(seconds: 3), () {
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const MainContainer()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox.expand(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.white, Color(0xFFE8F5E9)],
+            ),
+          ),
+          child: Center(
+            child: AnimatedScale(
+              scale: _scale,
+              duration: const Duration(milliseconds: 1500),
+              curve: Curves.fastOutSlowIn,
+              child: AnimatedOpacity(
+                opacity: _opacity,
+                duration: const Duration(milliseconds: 1000),
+                child: Image.asset(
+                  'assets/MoneyWise.png',
+                  // This makes the logo cover half the width, but center of the full screen
+                  width: double.infinity,
+                  height: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
