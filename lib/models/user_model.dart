19ab2afea2e1058e5d5d3d64a4760679b130d28e
kamau-David <dayvieek@gmail.com>
2026-01-31 23:48:29 +0300

    added user model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  final String name;
+  final String email;
+  final String userId;
+  final String memberSince;
+
+  UserModel({
+    required this.name,
+    required this.email,
+    required this.userId,
+    required this.memberSince,
+  });
+}
