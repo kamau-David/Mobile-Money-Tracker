01d1e28d81109c0072f038bf5ab1df2dba73d6ed
kamau-David <dayvieek@gmail.com>
2026-02-02 22:20:50 +0300

    Fix Flutter Gradle configuration and regenerate platform assets

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
