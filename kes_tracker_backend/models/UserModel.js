18f900577b17d8837e85e67d7035abbb8925e40d
kamau-David <dayvieek@gmail.com>
2026-03-12 17:39:34 +0300

    handles signup and sign in

--- kes_tracker_backend/models/UserModel.js
+++ kes_tracker_backend/models/UserModel.js
@@ -1,20 +1,21 @@
 const { pool } = require("../config/db");
 
 const User = {
-  create: async (phone, hashedPassword) => {
+  create: async (userData) => {
+    const { fullName, email, phone, hashedPassword } = userData;
     const query = `
-      INSERT INTO users (phone_number, password_hash)
-      VALUES ($1, $2) RETURNING id, phone_number;
+      INSERT INTO users (full_name, email_address,phone_number, password_hash)
+      VALUES ($1, $2) RETURNING id, full_name, email_address, phone_number;
     `;
-    const { rows } = await pool.query(query, [phone, hashedPassword]);
+    const values = [fullName, email, phone, hashedPassword];
+    const { rows } = await pool.query(query, values);
     return rows[0];
   },
 
-  findByPhone: async (phone) => {
-    const { rows } = await pool.query(
-      "SELECT * FROM users WHERE phone_number = $1",
-      [phone],
-    );
+  findById: async (id) => {
+    const query =
+      "SELECT id, full_name, email_address, phone_number FROM users WHERE id = $1";
+    const { rows } = await pool.query(query, [id]);
     return rows[0];
   },
 };
