502914bba6b40f99d2f0bad956b1d2a6bc0596b0
kamau-David <dayvieek@gmail.com>
2026-03-14 11:24:44 +0300

    destructered sms controller routes

--- kes_tracker_backend/routes/smsRoutes.js
+++ kes_tracker_backend/routes/smsRoutes.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const smsController = require("../controllers/smsController");
 
-router.post("/parse-sms", smsController.parseAndSaveSMS);
-router.get("/transactions", smsController.getAllTransactions);
-router.get("/summary", smsController.getFinanceSummary);
+// Destructure ONLY the SMS controller
+const {
+  parseAndSaveSMS,
+  getAllTransactions,
+  getFinanceSummary,
+} = require("../controllers/smsController");
+
+const { protect } = require("../middleware/authMiddleware");
+const reportController = require("../controllers/reportController");
+
+router.post("/parse-sms", protect, parseAndSaveSMS);
+router.get("/transactions", protect, getAllTransactions);
+router.get("/summary", protect, getFinanceSummary);
+
+router.get("/download-report", protect, reportController.generatePDF);
 
 module.exports = router;
